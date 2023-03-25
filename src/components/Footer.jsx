0c59e98b2b60400a32ec42a52f62e35e6a1ac903
DakshCodes <honeysingh617522@gmail.com>
2023-03-25 11:40:30 +0530

    Protfolio 3rd added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillYoutube,
+  AiOutlineArrowUp,
+} from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <img
+          src=''
+          alt="Founder"
+        />
+
+        <h2>Daksh Singh</h2>
+        <p>Motivation is temporary, but discipline last forever.</p>
+      </div>
+
+      <aside>
+        <h2>Social Media</h2>
+
+        <article>
+          <a href="" target={"blank"}>
+            <AiFillYoutube />
+          </a>
+          <a href="" target={"blank"}>
+            <AiFillInstagram />
+          </a>
+          <a href="" target={"blank"}>
+            <AiFillGithub />
+          </a>
+        </article>
+      </aside>
+      <a href="#home">
+        <AiOutlineArrowUp />
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
